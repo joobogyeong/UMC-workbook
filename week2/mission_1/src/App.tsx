2c97d5b5d90a5360832650cd279df8e07a171085
joobogyeong <powerbodol01@gmail.com>
2025-03-31 16:32:59 +0900

    tailwindcss 추가

--- week2/mission_1/src/App.tsx
+++ week2/mission_1/src/App.tsx
@@ -1,11 +1,13 @@
-import './App.css'
+import { useState } from 'react';
 
 function App() {
-
+  const [count, setCount] = useState(0);
   return (
     <>
-    <h1>hello world</h1>
+      <h1>{count}</h1>
+      <button onClick={()=>setCount(count+2)} style={{width:'200px', height:'200px', color:'black', backgroundColor:'green'}}>click!</button>
     </>
-  )
-}
-export default App
+  );
+};
+
+export default App;
